79df1eecad952b3d6b300970115af7d36ac6ca83
Anton Cheng <antonassocareer@gmail.com>
2020-05-05 14:30:18 +0800

    Add options

--- src/apis/bitcoin.ts
+++ src/apis/bitcoin.ts
@@ -1,31 +1,35 @@
-import request from './request'
+import request from './request';
 
-const pre = process.env.REACT_APP_PREFIX || ''
+const pre = process.env.REACT_APP_PREFIX || '';
 
-export async function getBalanceBlockInfo (address: string) {
-  const url = `${pre}https://blockchain.info/multiaddr?active=${address}`
-  return await request(url)
+export async function getBalanceBlockInfo(address: string) {
+  const init = {
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'Content-Type' },
+    method: 'GET',
+  };
+  const url = `${pre}https://blockchain.info/multiaddr?active=${address}`;
+  return await request(url, init);
 }
 
-export async function getBalanceOwnNode (address: string) {
+export async function getBalanceOwnNode(address: string) {
   const init = {
     headers: {
       'Content-Type': 'application/json',
-			Accept: 'application/json',
-			authKey: process.env.REACT_APP_CBX_PROXY_KEY,
-    }
-  }
-  const url = `https://route.cbx.io/api/proxy/btc/addr/${address}`
-  return await request(url, init)
+      Accept: 'application/json',
+      authKey: process.env.REACT_APP_CBX_PROXY_KEY,
+    },
+  };
+  const url = `https://route.cbx.io/api/proxy/btc/addr/${address}`;
+  return await request(url, init);
 }
 
 export async function getUSDTBalance(address: string) {
   const paramStr = `addr=${address}`;
   const init = {
-    headers:{ 'Content-Type': 'application/x-www-form-urlencoded' },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     method: 'POST',
-    body: paramStr
-  }
-  const url = `https://api.omniexplorer.info/v2/address/addr/`
-  return await request(url, init)
+    body: paramStr,
+  };
+  const url = `https://api.omniexplorer.info/v2/address/addr/`;
+  return await request(url, init);
 }
